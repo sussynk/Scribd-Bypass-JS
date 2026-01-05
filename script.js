2696bd3f52a5687722fa0ff8bb2f15a69b952cd3
Clement <119944151+sussynk@users.noreply.github.com>
2026-01-05 17:16:28 +0800

    temp invert color for debug

--- script.js
+++
@@ -1,6 +0,0 @@
-// alert("This tools is for educational purposes ONLY! \nThe author is not responsible for any misused or legal consequences.");
-
-let scribd_original_link = document.getElementById('scribd_original_link');
-
-
-
